d481739ff0904acca351666e495a41cdf93891c2
david <david.vilares89@gmail.com>
2015-12-02 11:21:31 +0800

    Dummy commit from eclipse.

---
+++ babelfy.py
@@ -0,0 +1,17 @@
+'''
+Created on 02/12/2015
+
+@author: david
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
