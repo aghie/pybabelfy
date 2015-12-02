0d158e9bda9fc5a7add7359c9d96b4facf0c921e
david <david.vilares89@gmail.com>
2015-12-02 15:32:49 +0800

    Added classes for parameter values

--- pybabelfy/babelfy.py
+++ pybabelfy/babelfy.py
@@ -7,7 +7,6 @@ Created on 02/12/2015
 import urllib
 import json
 
-
 class BabelfyJSONKeys(object):
     
     
@@ -22,12 +21,86 @@ class BabelfyJSONKeys(object):
     COHERENCE_SCORE = "coherenceScore" 
     GLOBAL_SCORE ="globalScore"
     SOURCE =  "source"
-           
+
+class AnnTypeValues(object):
     
-#     def expression(self):
-#         start = self.char_fragment()[self.CHAR_FRAGMENT_START]
-#         end = self.char_fragment()[self.CHAR_FRAGMENT_END]
-#         return self.text[start:end+1]
+    ALL = "ALL" #Disambiguates all
+    CONCEPTS = "CONCEPTS" #Disambiguates concepts only
+    NAMED_ENTITIES = "NAMED_ENTITIES" #Disambiguates named entities only
+    
+class AnnResValues(object):
+    BN = "BN" #Annotate with BabelNet synsets
+    WIKI = "WIKI" #Annotate with Wikipedia page titles
+    WN = "WN" #Annotate with WordNet synsets
+
+class MatchValues(object):
+    EXACT_MATCHING = "EXACT MATCHING" #Only exact matches are considered for disambiguation
+    PARTIAL_MATCHING = "PARTIAL MATCHING" #Both exact and partial matches (e.g.
+    
+class MCSValues(object):
+    OFF = "OFF" #Do not use Most Common Sense
+    ON = "ON" #Use Most Common Sense
+    ON_WITH_STOPWORDS = "ON_WITH_STOPWORDS" #Use Most Common Sense even on Stopwords
+
+class CandsValues(object):
+    ALL = "ALL" #Return all candidates for a fragment.
+    TOP = "TOP" #Return only the top ranked candidate for a fragment.
+
+class PosTagValues(object):
+    #Tokenize the input string by splitting all characters as single tokens 
+    #(all tagged as nouns, so that we can disambiguate nouns).
+    CHAR_BASED_TOKENIZATION_ALL_NOUN = "CHAR_BASED_TOKENIZATION_ALL_NOUN" 
+    INPUT_FRAGMENTS_AS_NOUNS = "INPUT_FRAGMENTS_AS_NOUNS" #Interprets input fragment words as nouns.
+    NOMINALIZE_ADJECTIVES = "NOMINALIZE_ADJECTIVES"  #Interprets all adjectives as nouns.
+    STANDARD = "STANDARD" #Standard PoS tagging process.
+
+class SemanticAnnotation(object):
+           
+    def __init__(self,babelfy_dict):
+        self.babelfy_dict = babelfy_dict
+    
+    def babelfy_dict(self):
+        return self.babelfy_dict    
+    
+    def token_fragment(self):
+        return self.babelfy_dict[BabelfyJSONKeys.TOKEN_FRAGMENT]
+    
+    def char_fragment(self):
+        return self.babelfy_dict[BabelfyJSONKeys.CHAR_FRAGMENT]
+    
+    def char_fragment_start(self):
+        return self.char_fragment()[BabelfyJSONKeys.CHAR_FRAGMENT_START]
+    
+    def char_fragment_end(self):
+        return self.char_fragment()[BabelfyJSONKeys.CHAR_FRAGMENT_END]
+    
+    def babel_synset_id(self):
+        return self.babelfy_dict[BabelfyJSONKeys.BABEL_SYNSET_ID]
+    
+    def dbpedia_url(self):
+        return self.babelfy_dict[BabelfyJSONKeys.DBPEDIA_URL]
+    
+    def babelnet_url(self):
+        return self.babelfy_dict[BabelfyJSONKeys.BABELNET_URL]
+    
+    def coherence_score(self):
+        return self.babelfy_dict[BabelfyJSONKeys.COHERENCE_SCORE]
+    
+    def global_score(self):
+        return self.babelfy_dict[BabelfyJSONKeys.GLOBAL_SCORE]
+    
+    def source(self):
+        return self.babelfy_dict[BabelfyJSONKeys.SOURCE]
+    
+    def postag(self):
+        return self.babel_synset_id()[-1]
+
+    
+    def pprint(self):
+        print self.babel_synset_id()
+        print self.babelnet_url()
+        print self.dbpedia_url()
+        print self.source()
 
 class Babelfy(object):
 
@@ -53,12 +126,7 @@ class Babelfy(object):
     def disambiguate(self, text,lang,key, anntype=None, annres=None, th=None,
                      match=None,mcs=None,dens=None,cands=None,postag=None,
                      extaida=None):
-        """
-        Params: Take a look to: http://babelfy.org/guide
-        @return: A list of dictionaries with the expressions that were disambiguated. List is sorted
-        according to the expressions position inside the text
-        """
-        dict_word_babelfy_json = {}
+        
         values = [text,lang,key,anntype,annres,th,match,mcs,dens,
                             cands,postag,extaida]
         
@@ -68,4 +136,4 @@ class Babelfy(object):
         
         json_string = urllib.urlopen(self.API+self.DISAMBIGUATE+query).read()
         babelfy_jsons = json.loads(json_string)
-        return babelfy_jsons
+        return [SemanticAnnotation(babelfy_json) for babelfy_json in babelfy_jsons]
