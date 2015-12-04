01494d5c9bf0163f890e494a70d38205fbc24c62
david <david.vilares89@gmail.com>
2015-12-04 10:33:27 +0800

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+'''
+Created on 02/12/2015
+
+@author: david
+'''
+
+from distutils.core import setup
+
+
+setup(
+  name = 'pybabelfy',
+  packages = ['pybabelfy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A Python interface for Babelfy',
+  author = 'David Vilares',
+  author_email = 'david.vilares89@gmail.com',
+  url = 'https://github.com/aghie/pybabelfy', # use the URL to the github repo
+  download_url = 'https://github.com/aghie/pybabelfy/tarball/0.1', # I'll explain this in a second
+  keywords = ['babelfy', 'wrapper', 'api'], # arbitrary keywords
+  classifiers = [],
+)
